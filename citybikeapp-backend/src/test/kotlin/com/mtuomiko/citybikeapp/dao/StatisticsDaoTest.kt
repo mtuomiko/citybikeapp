277650e46dcadad166eaff047f9bb12105ab9b54
Matti Tuomikoski <mtuomikoski@gmail.com>
2023-01-27 03:43:13 +0200

    Fix: never return more top stations than specified
    * Self referring top stations query result (departing and arriving stations are the same one) could result in more stations returned than specified.

--- citybikeapp-backend/src/test/kotlin/com/mtuomiko/citybikeapp/dao/StatisticsDaoTest.kt
+++ citybikeapp-backend/src/test/kotlin/com/mtuomiko/citybikeapp/dao/StatisticsDaoTest.kt
@@ -14,7 +14,7 @@ class StatisticsDaoTest {
     private val statisticsDao = StatisticsDao(journeyRepository)
 
     @Test
-    fun `Top stations are split and sorted according to arriving and departing station ids and journey count `() {
+    fun `Top stations are split and sorted according to arriving and departing station ids and journey count`() {
         val testStationId = 1
         val testQueryResults = List(6) {
             TopStationsQueryResultBuilder().departureStationId(testStationId).arrivalStationId(100 + it)
@@ -26,12 +26,12 @@ class StatisticsDaoTest {
                 .build()
         }
 
-        every { journeyRepository.getTopStationsByStationId(testStationId) } returns CompletableDeferred(
+        every { journeyRepository.getTopStationsByStationId(testStationId, 6) } returns CompletableDeferred(
             testQueryResults
         )
 
         runBlocking {
-            val result = statisticsDao.getTopStationsByStationId(testStationId, null, null)
+            val result = statisticsDao.getTopStationsByStationId(testStationId, null, null, 6)
 
             val expectedStationIdsWhereDepartedFrom =
                 testQueryResults.filter { it.arrivalStationId == testStationId }.sortedByDescending { it.journeyCount }
@@ -43,14 +43,45 @@ class StatisticsDaoTest {
 
             // expect descending order
             assertThat(result.forArrivingHere).hasSize(6)
-            assertThat(result.forArrivingHere).isSortedAccordingTo { o1, o2 -> (o2.journeyCount - o1.journeyCount).toInt() }
+            assertThat(result.forArrivingHere).isSortedAccordingTo { o1, o2 ->
+                (o2.journeyCount - o1.journeyCount).toInt()
+            }
             assertThat(result.forArrivingHere.map { it.id }).containsExactlyElementsOf(
                 expectedStationIdsWhereDepartedFrom
             )
 
             assertThat(result.forDepartingTo).hasSize(6)
-            assertThat(result.forDepartingTo).isSortedAccordingTo { o1, o2 -> (o2.journeyCount - o1.journeyCount).toInt() }
+            assertThat(result.forDepartingTo).isSortedAccordingTo { o1, o2 ->
+                (o2.journeyCount - o1.journeyCount).toInt()
+            }
             assertThat(result.forDepartingTo.map { it.id }).containsExactlyElementsOf(expectedStationIdsWhereArrivedTo)
+        }
+    }
+
+    @Test
+    fun `Given query result with self referring top station, top station response does not exceed count limit`() {
+        val testStationId = 1
+        val testQueryResults = listOf(
+            TopStationsQueryResultBuilder().departureStationId(2).arrivalStationId(testStationId)
+                .journeyCount(10).build(),
+            TopStationsQueryResultBuilder().departureStationId(testStationId).arrivalStationId(testStationId)
+                .journeyCount(5).build()
+        )
+
+        every { journeyRepository.getTopStationsByStationId(testStationId, 1) } returns CompletableDeferred(
+            testQueryResults
+        )
+
+        runBlocking {
+            val result = statisticsDao.getTopStationsByStationId(testStationId, null, null, 1)
+
+            assertThat(result.forArrivingHere).hasSize(1)
+            assertThat(result.forArrivingHere.first().id).isEqualTo(2)
+            assertThat(result.forArrivingHere.first().journeyCount).isEqualTo(10)
+
+            assertThat(result.forDepartingTo).hasSize(1)
+            assertThat(result.forDepartingTo.first().id).isEqualTo(testStationId)
+            assertThat(result.forDepartingTo.first().journeyCount).isEqualTo(5)
         }
     }
 }
